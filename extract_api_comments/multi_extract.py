458af886595bfda710e3fac3b3a9af7e089332b0
xiaoyue10131748 <xiaoyue10131748@gmail.com>
2021-01-16 22:23:06 -0500

    fix decode

--- extract_api_comments/multi_extract.py
+++ extract_api_comments/multi_extract.py
@@ -4,9 +4,9 @@ import sys
 from extract_api_comments import  getDescriptions
 
 def worker(flag, version):
-    t_start = time.time()  # 获取当前系统时间，长整型，常用来测试程序执行时间
-    print("%s开始执行,进程号为%d" % (version, os.getpid()))
-    # random.random()随机生成0~1之间的浮点数
+    t_start = time.time()
+    print("%sbegin excuting %d" % (version, os.getpid()))
+
     if flag == "linux":
         version_path = "../../source_code/Linux/" + version
         topath = "../../data/Linux/" + version + "/"
@@ -20,7 +20,7 @@ def worker(flag, version):
 
     time.sleep(random.random() * 2)
     t_stop = time.time()
-    print(version, "执行完毕，耗时%0.2f" % (t_stop - t_start))
+    print(version, "excute end %0.2f" % (t_stop - t_start))
 
 
 def main():
@@ -30,7 +30,7 @@ def main():
 
     #path = "../../source_code/Linux/"
 
-    po = Pool(3)  # 定义一个进程池，最大进程数3，大小可以自己设置，也可写成processes=3
+    po = Pool(10)
     versions = os.listdir(path)
     for i in range(len(versions)):
         # Pool().apply_async(要调用的目标,(传递给目标的参数元祖,))
